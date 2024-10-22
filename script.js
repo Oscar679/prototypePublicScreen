9e0d2fd3d347b77bb33d4b2b0c6c560927e9b671
Oscar Ekberg <oscarekberg@hotmail.com>
2024-10-22 16:31:23 +0200

    added a bunch of graphics and smaller functionality for counters

--- script.js
+++ script.js
@@ -1,15 +1,45 @@
 document.addEventListener("DOMContentLoaded", function () {
     let participateBtn = document.querySelector("#participateBtn");
     let counter = document.querySelector("#counter");
+    let counterYesterday = document.querySelector("#counterYesterday");
+    let resElem = document.querySelector("#resElem");
 
     participateBtn.addEventListener("click", addToCounter);
 
+    getData();
+    compareParticipation();
+
     function addToCounter() {
-        let currentCounter = parseInt(counter.textContent);
+        let currentCounter = counter.textContent;
         currentCounter++;
         console.log(currentCounter);
         counter.textContent = currentCounter;
+
+        saveData(currentCounter);
+        compareParticipation();
     }
 
-    console.log(participateBtn);
+    function saveData(currentCounter) {
+        let dataStr = encodeURIComponent(currentCounter);
+        localStorage.setItem("counterData", dataStr);
+    }
+
+    function getData() {
+        let dataStr = localStorage.getItem("counterData");
+        if (dataStr == null) return;
+
+        counter.textContent = dataStr;
+    }
+
+    function compareParticipation() {
+        let difference = parseInt(counterYesterday.textContent) - parseInt(counter.textContent);
+        console.log(difference);
+        console.log
+        if (difference <= 0 || difference == null) {
+            resElem.innerHTML = "";
+            return;
+        } else {
+            resElem.innerHTML = "<p>" + "Det behvös endast " + difference + " deltagare till för att slå gårdagens resultat!" + "</p>";
+        }
+    }
 });
